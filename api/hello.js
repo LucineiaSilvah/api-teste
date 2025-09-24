75c3de6f16955f71f393b666531e3168d6b27d20
luh <lucineia.rsilvah@gmail.com>
2025-09-24 20:57:38 -0300

    update

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,11 @@
-export default function handler(req, res) {
+import express from 'express';
+const app = express();
+
+app.get('/api/hello', (req, res) => {
   res.status(200).json({ mensagem: 'Olá, Lucineia!' });
-}
+});
+
+export default app; // Exporta o app para ser usado pelo servidor Vercel
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
